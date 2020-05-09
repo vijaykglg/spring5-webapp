93778e26a565dbfdc85b6a98664134f097567084
vijaykglg <vijay.gupta7@yahoo.in>
2020-05-09 11:23:06 +0530

    First Commit

---
+++ src/main/java/com/vijay/learn/spring/spring5webapp/Spring5WebappApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.learn.spring.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebappApplication.class, args);
+    }
+
+}
